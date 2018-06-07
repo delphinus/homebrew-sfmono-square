d94356e4f1a56747b83c3b8f1b04f8b0e410b47e
delphinus <delphinus@remora.cx>
2018-06-07 15:48:36 +0900

    Bump version up to v0.1.2018060708

--- sfmono-square.rb
+++ sfmono-square.rb
@@ -1,10 +1,10 @@
 class SfmonoSquare < Formula
   desc "Square-sized SF Mono + Japanese fonts + nerd-fonts"
   homepage "https://github.com/delphinus/homebrew-sfmono-square"
-  url "https://github.com/delphinus/homebrew-sfmono-square/archive/v0.1.2018060707.tar.gz"
-  sha256 "37cebaa7755c9ad9fa34c153b219561337c7a198f89bf8951a6725db495bfe8f  -
+  url "https://github.com/delphinus/homebrew-sfmono-square/archive/v0.1.2018060708.tar.gz"
+  sha256 "5a5d8695c194e3bd72265018c3c36d49773a588f12df3579318929bd36a2d934  -
 "
-  version "0.1.2018060707"
+  version "0.1.2018060708"
   depends_on "fontforge" => :build
   depends_on "python@2" => :build
 
