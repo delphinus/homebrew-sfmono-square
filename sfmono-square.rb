30ffc25fa977cb1041e311b83d46e4be30bc6ccb
delphinus <delphinus@remora.cx>
2018-06-19 14:27:56 +0900

    Use GNU parallel to create fonts faster

--- sfmono-square.rb
+++ sfmono-square.rb
@@ -4,7 +4,9 @@ class SfmonoSquare < Formula
   url "https://github.com/delphinus/homebrew-sfmono-square/archive/v0.1.2018061801.tar.gz"
   sha256 "2f76835fe84847861b89e56c481fcaba87bab77dbb61be1e8a655e9dcf412b90"
   version "0.1.2018061801"
+
   depends_on "fontforge" => :build
+  depends_on "parallel" => :build
   depends_on "python@2" => :build
 
   resource "sfmono" do
@@ -21,18 +23,49 @@ class SfmonoSquare < Formula
     resource("sfmono").stage { buildpath.install Dir["*"] }
     resource("migu1mfonts").stage { buildpath.install Dir["*"] }
 
-    system "fontforge", "-script", buildpath/"src/generate-oblique.pe"
-    system "fontforge", "-script", buildpath/"src/modify-migu1m.pe"
-    system "fontforge", "-script", buildpath/"src/modify-sfmono.pe"
-    system "fontforge", "-script", buildpath/"src/generate-sfmono-mod.pe"
-    Dir[
+    # generate-oblique
+    system(
+      "parallel", "-N3", "fontforge", "-script", buildpath/"src/generate-oblique.pe",
+      ":::",
+      "migu-1m-regular.ttf", "Regular", "regular",
+      "migu-1m-bold.ttf",    "Bold",    "bold",
+    )
+    system(
+      "parallel", "-N2", "fontforge", "-script", buildpath/"src/modify-migu1m.pe",
+      ":::",
+      "migu-1m-regular.ttf",      "modified-migu-1m-regular.ttf",
+      "migu-1m-bold.ttf",         "modified-migu-1m-bold.ttf",
+      "migu-1m-oblique.ttf",      "modified-migu-1m-oblique.ttf",
+      "migu-1m-bold-oblique.ttf", "modified-migu-1m-bold-oblique.ttf",
+    )
+    system(
+      "parallel", "-N2", "fontforge", "-script", buildpath/"src/modify-sfmono.pe",
+      ":::",
+      "SFMono-Regular.otf",       "SFMonoSquare-Regular.otf",
+      "SFMono-Bold.otf",          "SFMonoSquare-Bold.otf",
+      "SFMono-RegularItalic.otf", "SFMonoSquare-RegularItalic.otf",
+      "SFMono-BoldItalic.otf",    "SFMonoSquare-BoldItalic.otf",
+    )
+    system(
+      "parallel", "-N4", "fontforge", "-script", buildpath/"src/generate-sfmono-mod.pe",
+      ":::",
+      "SFMonoSquare-Regular.otf",       "modified-migu-1m-regular.ttf",      "Regular",     "Regular",
+      "SFMonoSquare-Bold.otf",          "modified-migu-1m-bold.ttf",         "Bold",        "Bold",
+      "SFMonoSquare-RegularItalic.otf", "modified-migu-1m-oblique.ttf",      "Italic",      "RegularItalic",
+      "SFMonoSquare-BoldItalic.otf",    "modified-migu-1m-bold-oblique.ttf", "Bold Italic", "BoldItalic",
+    )
+    otfs = Dir[
       "SFMonoSquare-Regular.otf",
       "SFMonoSquare-Bold.otf",
       "SFMonoSquare-RegularItalic.otf",
       "SFMonoSquare-BoldItalic.otf",
-    ].each do |ttf|
-      system "fontforge", "-lang=py", "-script", buildpath/"src/font-patcher", "-q", "-out", "build", "--square", buildpath/ttf
-    end
+    ].map { |f| buildpath/f }
+    system(
+      "parallel", "fontforge", "-lang=py", "-script",
+      buildpath/"src/font-patcher", "-q", "-out", "build", "--square",
+      ":::",
+      *otfs,
+    )
     (share/"fonts").install Dir["build/*.otf"]
   end
 end
