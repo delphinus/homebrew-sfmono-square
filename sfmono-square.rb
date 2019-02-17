9b775d09f94e9aa3b4b46268392ceec4de3e6ac1
delphinus <delphinus@remora.cx>
2019-02-17 20:49:00 +0900

    Use SF Mono from a local path instead of fetching

--- sfmono-square.rb
+++ sfmono-square.rb
@@ -9,11 +9,6 @@ class SfmonoSquare < Formula
   depends_on "fontforge" => :build
   depends_on "python@2" => :build
 
-  resource "sfmono" do
-    url "https://github.com/ZulwiyozaPutra/SF-Mono-Font/archive/c18d1be46975d55039c3f058cf148742ddeb2723.zip"
-    sha256 "d42e21f0696df0be75ea99ab669135ec5cf757b5f3fcf002abf49d284da7b0b8"
-  end
-
   resource "migu1mfonts" do
     url "https://osdn.jp/frs/redir.php?m=gigenet&f=%2Fmix-mplus-ipa%2F63545%2Fmigu-1m-20150712.zip"
     sha256 "d4c38664dd57bc5927abe8f4fbea8f06a8ece3fea49ea02354d4e03ac6d15006"
@@ -26,9 +21,11 @@ class SfmonoSquare < Formula
   end
 
   def install
-    resource("sfmono").stage { buildpath.install Dir["*"] }
     resource("migu1mfonts").stage { buildpath.install Dir["*"] }
     resource("futures").stage { buildpath.install Dir["*"] }
+
+    sfmono_dir = Pathname.new '/Applications/Utilities/Terminal.app/Contents/Resources/Fonts'
+    cp Pathname.glob(sfmono_dir/'*'), buildpath
 
     system buildpath/"bin/sfmono-square", version
     (share/"fonts").install Dir["build/*.otf"]
