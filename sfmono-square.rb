8556f3fd24d706eef644346787277d563e645414
delphinus <me@delphinus.dev>
2019-06-13 18:06:14 +0900

    Remove futures to use the native one in Python3

--- sfmono-square.rb
+++ sfmono-square.rb
@@ -9,22 +9,15 @@ class SfmonoSquare < Formula
   head 'https://github.com/delphinus/homebrew-sfmono-square.git'
 
   depends_on 'fontforge' => :build
-  depends_on 'python@2' => :build
+  depends_on 'python' => :build
 
   resource 'migu1mfonts' do
     url 'https://osdn.jp/frs/redir.php?m=gigenet&f=%2Fmix-mplus-ipa%2F63545%2Fmigu-1m-20150712.zip'
     sha256 'd4c38664dd57bc5927abe8f4fbea8f06a8ece3fea49ea02354d4e03ac6d15006'
   end
 
-  # made by homebrew-pypi-poet
-  resource 'futures' do
-    url 'https://files.pythonhosted.org/packages/1f/9e/7b2ff7e965fc654592269f2906ade1c7d705f1bf25b7d469fa153f7d19eb/futures-3.2.0.tar.gz'
-    sha256 '9ec02aa7d674acb8618afb127e27fde7fc68994c0437ad759fa094a574adb265'
-  end
-
   def install
     resource('migu1mfonts').stage { buildpath.install Dir['*'] }
-    resource('futures').stage { buildpath.install Dir['*'] }
 
     sfmono_dir = Pathname.new '/Applications/Utilities/Terminal.app/Contents/Resources/Fonts'
     [
