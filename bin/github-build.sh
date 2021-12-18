f0ee7d17b7e26b71cb1a5873c46853ee120992c4
delphinus <me@delphinus.dev>
2021-12-18 12:16:24 +0900

    Use GITHUB_SHA instead of calculating myself

--- bin/github-build.sh
+++ bin/github-build.sh
@@ -11,13 +11,11 @@ else
   # TODO: avoid errors for this
   brew install --only-dependencies sfmono-square || true
   FORMULA=sfmono-square.rb
-  HASH=$(git describe --tags HEAD | tr -d '\n')
-  export HASH
-  export URL=https://github.com/delphinus/homebrew-sfmono-square/archive/$HASH.tar.gz
+  export URL=https://github.com/delphinus/homebrew-sfmono-square/archive/$GITHUB_SHA.tar.gz
   SHA=$(curl -L "$URL" | shasum -a256 | cut -f1 -d ' ')
   export SHA
   perl -i -pe 's,(?<=^  url ").*(?="$),$ENV{URL},' $FORMULA
   perl -i -pe 's,(?<=^  sha256 ").*(?="$),$ENV{SHA},' $FORMULA
-  perl -i -pe 's,(?<=^  version ").*(?="$),$ENV{HASH},' $FORMULA
+  perl -i -pe 's,(?<=^  version ").*(?="$),$ENV{GITHUB_SHA},' $FORMULA
   brew install -vd --build-from-source $FORMULA
 fi
