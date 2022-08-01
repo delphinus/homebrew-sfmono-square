db66bacc5f85c10bd8da5a44048b9e01b4343480
delphinus <me@delphinus.dev>
2022-08-01 13:28:05 +0900

    Use Python 3.10 because fontforge use that now

--- sfmono-square.rb
+++ sfmono-square.rb
@@ -9,7 +9,7 @@ class SfmonoSquare < Formula
   head "https://github.com/delphinus/homebrew-sfmono-square.git"
 
   depends_on "fontforge" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
 
   resource "migu1mfonts" do
     output, = system_command curl_executable,
@@ -47,9 +47,9 @@ class SfmonoSquare < Formula
     # ENV["MIGU1M_SCALE"] = "82"
 
     # Set path for fontforge library to use it in Python
-    fontforge_lib = Formulary.factory("fontforge").lib / "python3.9/site-packages"
+    fontforge_lib = Formulary.factory("fontforge").lib / "python3.10/site-packages"
 
-    python39 = Formulary.factory("python@3.9").bin / "python3"
+    python39 = Formulary.factory("python@3.10").bin / "python3"
 
     system python39, "-c", <<~PYTHON
       import sys
