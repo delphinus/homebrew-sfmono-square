b85fc1a9588775316b17c54180c7cc3f67da9da8
JINNOUCHI Yasushi <me@delphinus.dev>
2022-10-25 11:24:40 +0900

    Fix to detect master validly

--- bin/github-build.sh
+++ bin/github-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 sw_vers # show macOS version
 brew update
-if [[ $GITHUB_REF = master ]]; then
+if [[ $GITHUB_REF = refs/heads/master ]]; then
   : build on the latest tag
   brew tap delphinus/sfmono-square
   brew install sfmono-square
