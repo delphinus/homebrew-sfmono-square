d108d381e706f1156bdfdac4b21d8584ab5a63d3
delphinus <me@delphinus.dev>
2022-10-27 17:54:45 +0900

    Fix alignments

--- bin/github-build.sh
+++ bin/github-build.sh
@@ -7,16 +7,16 @@ if [[ $GITHUB_REF = refs/heads/master ]]; then
   brew install sfmono-square
 else
   : build on "$GITHUB_REF"
-  FORMULA=sfmono-square.rb
-  # TODO: avoid errors for this
-  brew install --only-dependencies $FORMULA || true
   HASH=$(git describe --tags HEAD | tr -d '\n')
   export HASH
   export URL=https://github.com/delphinus/homebrew-sfmono-square/tarball/$HASH
   SHA=$(curl -L "$URL" | shasum -a256 | cut -f1 -d ' ')
   export SHA
+  FORMULA=sfmono-square.rb
   perl -i -pe 's,(?<=^  url ").*(?="$),$ENV{URL},' $FORMULA
   perl -i -pe 's,(?<=^  sha256 ").*(?="$),$ENV{SHA},' $FORMULA
   perl -i -pe 's,(?<=^  version ").*(?="$),$ENV{HASH},' $FORMULA
+  # TODO: avoid errors for this
+  brew install --only-dependencies $FORMULA || true
   brew install -v $FORMULA
 fi
