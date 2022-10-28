f80cccc6f534f88cc9effe8f1d70fea33f0880c8
delphinus <me@delphinus.dev>
2022-10-28 13:06:55 +0900

    Check for created fonts

--- bin/github-build.sh
+++ bin/github-build.sh
@@ -20,3 +20,15 @@ else
   brew install --only-dependencies $FORMULA || true
   brew install -v $FORMULA
 fi
+
+font_dir=/usr/local/opt/sfmono-square/share/fonts
+
+for i in Bold BoldItalic Regular RegularItalic; do
+  font=$font_dir/SFMonoSquare-$i.otf
+  if [[ -f $font ]]; then
+    echo "found $font"
+  else
+    echo "cannot find $font" 2>&1
+    exit 1
+  fi
+done
