9c2b318eb573be8592b616036573a5468416ac67
delphinus <me@delphinus.dev>
2023-07-16 16:15:41 +0900

    hoge

--- src/sfmono_square.py
+++ src/sfmono_square.py
@@ -134,7 +134,7 @@ def new_font(opts):
     font.appendSFNTName("English (US)", "Family", "SF Mono Square SFNT Family")
     font.appendSFNTName("English (US)", "SubFamily", opts["style"])
     font.appendSFNTName(
-        "English (US)", "PreferredFamily", "SF Mono Square SFNT PreferredFamily"
+        "English (US)", "PreferredFamilyName", "SF Mono Square SFNT PreferredFamily"
     )
     font.appendSFNTName(
         "English (US)", "PreferredSubFamily", "SF Mono Square SFNT PreferredSubFamily"
