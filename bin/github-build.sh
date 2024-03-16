34c8d0fcbb356776694e1fa2395ca2f06d65cb3f
delphinus <me@delphinus.dev>
2024-03-16 14:20:47 +0900

    chore: fix ci error in Ventura

--- bin/github-build.sh
+++ bin/github-build.sh
@@ -22,10 +22,8 @@ else
   perl -i -pe 's,(?<=^  version ").*(?="$),$ENV{HASH},' $FORMULA
   os_version=$(sw_vers -productVersion | cut -d. -f1)
   if [[ $os_version -eq 13 ]]; then
-    if ! brew install -v --formula $FORMULA; then
-      # HACK: ignore strange errors for old macOS
-      echo 'failed to install formula, but ignored' >&2
-    fi
+    # HACK: ignore strange errors for old macOS
+    brew install -v --formula $FORMULA || true
   else
     brew install -v --formula $FORMULA
   fi
