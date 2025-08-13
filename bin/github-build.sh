32dcb6d0b27836938dea7906b2df7219177e61be
delphinus <me@delphinus.dev>
2025-08-13 23:24:41 +0900

    fix: use local tap to avoid CI errors

--- bin/github-build.sh
+++ bin/github-build.sh
@@ -4,11 +4,9 @@ export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 export HOMEBREW_NO_INSTALL_FROM_API=1
 sw_vers # show macOS version
-brew update
+FORMULA=sfmono-square.rb
 if [[ $GITHUB_REF = refs/heads/master ]]; then
   : build on the latest tag
-  brew tap delphinus/sfmono-square
-  brew install delphinus/sfmono-square/sfmono-square
 else
   : build on "$GITHUB_REF"
   HASH=$(git describe --tags HEAD | tr -d '\n')
@@ -16,12 +14,11 @@ else
   export URL=https://github.com/delphinus/homebrew-sfmono-square/tarball/$HASH
   SHA=$(curl -L "$URL" | shasum -a256 | cut -f1 -d ' ')
   export SHA
-  FORMULA=sfmono-square.rb
-  TAP=local/sfmono-square
   perl -i -pe 's,(?<=^  url ").*(?="$),$ENV{URL},' $FORMULA
   perl -i -pe 's,(?<=^  sha256 ").*(?="$),$ENV{SHA},' $FORMULA
   perl -i -pe 's,(?<=^  version ").*(?="$),$ENV{HASH},' $FORMULA
-  brew tap-new $TAP
-  cp $FORMULA "$(brew --repo $TAP)/Formula/"
-  brew install -v $TAP/sfmono-square
 fi
+TAP=local/sfmono-square
+brew tap-new $TAP
+cp $FORMULA "$(brew --repo $TAP)/Formula/"
+brew install -v $TAP/sfmono-square
